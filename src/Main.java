2773f63f47397065f188b6200f7ca84db1ad9760
jamesfrancos <dthiagovas@gmail.com>
2025-02-21 16:18:50 -0300

    Estágio final de criação,sujeito a futuros reparos.

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import br.com.dio.desafio.models.Funcionario;
+import br.com.dio.desafio.models.Gerente;
+import br.com.dio.desafio.models.Sistema;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       boolean desativar=false;
+       Scanner sc=new Scanner(System.in);
+       Sistema sistema=new Sistema();
+       try {
+           while (!desativar) {
+               System.out.println("");
+               System.out.println("SISTEMA- Qual Operacao e desejada?");
+               System.out.println("1-Cadastrar Funcionario");
+               System.out.println("2-Gerenciar Funcionario(jornada de trabalho e salario)");
+               System.out.println("digite outro numero caso queira encerrar a sessao");
+               int opcao = sc.nextInt();
+               switch (opcao) {
+                   case 1:
+                       sistema.cadastrarFuncionario();
+                       break;
+
+                   case 2:
+                       sistema.gerenciarFuncionario();
+                       break;
+
+                   default:
+                       System.out.println("Fim");
+                       desativar = true;
+                       break;
+               }
+           }
+       }catch (InputMismatchException e){
+           System.out.println("O Tipo de dado fornecido esta incorreto");
+       }
+    }
+}
